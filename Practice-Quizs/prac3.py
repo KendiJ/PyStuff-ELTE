97329f1655e0c3c9526a305a559127b62b5bbb2f
Kendi J <kendyjaky@gmail.com>
2025-12-04 23:48:54 +0100

    Practice 3

--- Practice-Quizs/prac3.py
+++ Practice-Quizs/prac3.py
@@ -4,6 +4,23 @@
 # Add a method called has_passed() that returns True if the average is 50 or higher, and False otherwise.
 # Test: Create a student named "John" with grades [40, 60, 50] and print if he passed.
 
+class Student:
+    def __init__ (self, name, grades):
+        self.name = name
+        self.grades = grades
 
-
+    def get_average(self):
+        if len(self.grades) == 0:
+            return 0
+        return sum(self.grades) / len(self.grades)
     
+    def has_passed(self):
+        if self.get_average() >= 50:
+            return True
+        else:
+            return False
+
+john = Student("John", [40, 66, 50])
+
+print(f"John's average: {john.get_average()}")
+print(f"Did he pass: {john.has_passed()}")
